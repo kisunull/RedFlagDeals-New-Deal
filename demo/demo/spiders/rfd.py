58f9fe59c5b015f26de1be8b84570ac95e0d667f
Kisu San <klu@live.com>
2024-12-23 22:29:01 -0500

    Update rfd.py

--- demo/demo/spiders/rfd.py
+++ demo/demo/spiders/rfd.py
@@ -1,64 +1,87 @@
 import scrapy
-import requests
 import configparser
 import tg
-from demo.items import RfdItem
-from scrapy.crawler import CrawlerProcess
+from demo.items import RfdItem  # Ensure RfdItem is properly defined in demo/items.py
 from urllib.parse import quote
 
 
 class RfdSpider(scrapy.Spider):
-	name = 'rfd'
-	allowed_domains = ['redflagdeals.com']
-	start_urls = ['https://forums.redflagdeals.com/hot-deals-f9/?sk=tt&rfd_sk=tt&sd=d']
+    name = 'rfd'
+    allowed_domains = ['redflagdeals.com']
+    start_urls = ['https://forums.redflagdeals.com/hot-deals-f9/?sk=tt&rfd_sk=tt&sd=d']
 
-	def parse(self, response):
-		######################################################################### 
-		#  Configuration
-		######################################################################### 
-		CONFIG_FILE='config_loader.ini'
-		CONFIG_MAIN='main'
-		configParser = configparser.ConfigParser()
-		configParser.read(CONFIG_FILE)
-		UID = configParser.get(CONFIG_MAIN, "uid") 
+    def parse(self, response):
+        #########################################################################
+        #  Configuration
+        #########################################################################
+        CONFIG_FILE = 'config_loader.ini'
+        CONFIG_MAIN = 'main'
+        configParser = configparser.ConfigParser()
+        configParser.read(CONFIG_FILE)
 
-		for s in response.xpath('//div[@class="forumbg"]/div/ul[@class="topiclist topics with_categories"]'):
-			item = RfdItem()
-			item['uid'] = s.xpath('./li/@data-thread-id').extract()
-			item['retailer'] = s.xpath('//h3/a[@class="topictitle_retailer"]/text()').extract()
-			item['title'] = s.xpath('//h3/a[@class="topic_title_link"]/text()').extract()
-			retailers = s.xpath('//h3[contains(@class,"topictitle")]/text()[1]').extract()
-			class_values = s.xpath('//h3[contains(@class,"topictitle")]/@class').extract()
-			#item['link'] = s.xpath('//h3/a[@class="topic_title_link"]/@href').extract()
-			#item['time'] = s.xpath('//span[@class="first-post-time"]/text()').extract()
+        # Retrieve the last processed UID from the configuration file
+        try:
+            old_uid = configParser.get(CONFIG_MAIN, "uid")
+        except configparser.NoOptionError:
+            old_uid = "0"  # Default to "0" if UID is not set
 
-		#set retailer couter
-		retailer_c = len(item['retailer']) - 1
+        # Initialize the maximum UID found in this scrape
+        max_uid = old_uid
 
-		for i in reversed(range(len(item['uid']))):
-			#get title
-			retailer = retailers[i].replace('\n','')
-			title = item['title'][i].replace('\n','')
+        #########################################################################
+        #  Extract all threads from the forum page
+        #########################################################################
+        # Each thread is contained within an <li> element with class "topic"
+        threads = response.xpath('//ul[contains(@class, "topiclist")]/li[contains(@class, "topic")]')
 
-			if retailer:
-				title = '%s%s' % (retailer, title)
-			elif 'topictitle_has_retailer' in class_values[i]:
-				title = '[%s] %s' % (item['retailer'][retailer_c], title)
-				retailer_c -= 1
+        for thread in threads:
+            # Initialize the item to store thread data
+            item = RfdItem()
 
-			#check uid
-			if item['uid'][i] > UID:
-				UID = item['uid'][i]
+            # Extract the thread UID from the data-thread-id attribute
+            uid = thread.xpath('./@data-thread-id').get()
+            if not uid:
+                continue  # Skip if UID is not found
 
-				title = quote(title, safe='')
-				link = 'https://forums.redflagdeals.com/viewtopic.php?t=%s' % item['uid'][i]
-				bot_message = '*%s*\n%s' % (title, link)
+            item['uid'] = uid
 
-				tg.bot_sendtext_channel(bot_message)
+            # Extract the retailer from the <a> tag with class "thread_dealer"
+            retailer = thread.xpath('.//a[contains(@class, "thread_dealer")]/span/text()').get()
+            retailer = retailer.strip() if retailer else ''
 
+            # Extract the thread title from the <a> tag with class "thread_title_link"
+            title = thread.xpath('.//h3[contains(@class, "thread_title")]/a[contains(@class, "thread_title_link")]/text()').get()
+            title = title.strip() if title else ''
 
-		# update uid to config file
-		configParser.set(CONFIG_MAIN, "uid", UID)
-		# save to the file
-		with open(CONFIG_FILE, 'w') as configfile:
-			configParser.write(configfile)
+            # Combine retailer and title if retailer exists
+            if retailer:
+                final_title = f"[{retailer}] {title}"
+            else:
+                final_title = title
+
+            #####################################################################
+            #  Determine if the thread is new by comparing UID
+            #####################################################################
+            if uid > old_uid:
+                # Update max_uid if the current UID is greater
+                if uid > max_uid:
+                    max_uid = uid
+
+                # Encode the title for URL and message safety
+                encoded_title = quote(final_title, safe='')
+
+                # Construct the thread link using the UID
+                link = f'https://forums.redflagdeals.com/viewtopic.php?t={uid}'
+
+                # Prepare the Telegram message
+                bot_message = f'*{encoded_title}*\n{link}'
+
+                # Send the message to Telegram channel
+                tg.bot_sendtext_channel(bot_message)
+
+        #########################################################################
+        #  Update the UID in the configuration file to the latest UID found
+        #########################################################################
+        configParser.set(CONFIG_MAIN, "uid", max_uid)
+        with open(CONFIG_FILE, 'w') as configfile:
+            configParser.write(configfile)
